f5ca85e1041c507b7c6d06318bdd1e05e709728e
alexredwood <redwoodalexander@gmail.com>
2019-10-20 23:53:15 +1100

    linting

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,92 @@
-import datastore from "./lib/datastore";
+import { Query } from "@google-cloud/datastore";
 import {queue} from "async";
 import fs from "fs";
-interface ISpace{
-    namespace:string,
-    kind:string,
+import datastore from "./lib/datastore";
+interface ISpace {
+    namespace: string;
+    kind: string;
 }
 
 const getSpaces = async () => {
     const namespaces = await datastore.getNamespaces(datastore.instance);
-    let operations : Promise<any>[] = [];
+    const operations: Array<Promise<any>> = [];
 
     // push default namespace
 
-    namespaces.forEach(namespace => {
-        const space = new Promise(async (res,rej) => {
-            const kinds = await datastore.getKindsInNamespace(datastore.instance,namespace);
+    namespaces.forEach((namespace) => {
+        const space = new Promise(async (res, rej) => {
+            const kinds = await datastore.getKindsInNamespace(datastore.instance, namespace);
             res({
+                kinds,
                 namespace,
-                kinds
-            })
-        })
+            });
+        });
         operations.push(space);
     });
 
-    const trees : {
-        namespace:string,
-        kinds:string[]
-    }[] = await Promise.all(operations);
+    const trees: Array<{
+        namespace: string,
+        kinds: string[],
+    }> = await Promise.all(operations);
 
-    let spaces:ISpace[] = [];
+    const spaces: ISpace[] = [];
 
-    trees.forEach(tree => {
-        tree.kinds.forEach(kind => {
+    trees.forEach((tree) => {
+        tree.kinds.forEach((kind) => {
             spaces.push({
-                namespace:tree.namespace,
-                kind
-            })
-        })
+                kind,
+                namespace: tree.namespace,
+            });
+        });
     });
 
     return spaces;
 
-}
+};
 
-const downloadDatastore = async (spaces:ISpace[]) => {
-    fs.mkdirSync("exports",{
-        recursive:true
+const downloadDatastore = async (spaces: ISpace[]) => {
+    fs.mkdirSync("exports", {
+        recursive: true,
     });
 
-    const downloadSpaceQueue = queue((space:ISpace,cb: () => void) => {
+    const downloadSpaceQueue = queue((space: ISpace, cb: () => void) => {
 
-        
-
-        let counter = 0;
-        fs.mkdirSync(`exports/${space.namespace}/${space.kind}`,{
-            recursive:true
+        fs.mkdirSync(`exports/${space.namespace}/${space.kind}`, {
+            recursive: true,
         });
-
-        if(space.namespace === "[default]"){
-            var query = datastore.instance.createQuery(space.kind);
+        let query: Query;
+        if (space.namespace === "[default]") {
+            query = datastore.instance.createQuery(space.kind);
+        } else {
+            query = datastore.instance.createQuery(space.namespace, space.kind);
         }
-        else{
-            var query = datastore.instance.createQuery(space.namespace,space.kind);
-        }
-
 
         const stream = datastore.instance.runQueryStream(query);
 
-        stream.on("data",(result) => {
+        stream.on("data", (result) => {
             const id = datastore.getId(result);
-            fs.writeFileSync(`exports/${space.namespace}/${space.kind}/${id}.json`,JSON.stringify(result,null,2),{
-                encoding:"utf8"
+            fs.writeFileSync(`exports/${space.namespace}/${space.kind}/${id}.json`, JSON.stringify(result, null, 2), {
+                encoding: "utf8",
             });
-            console.log(space,counter++);
         });
 
-        stream.on("end",() => {
+        stream.on("end", () => {
             cb();
-        })
+        });
 
     });
 
-
     downloadSpaceQueue.push(spaces);
-}
+};
 
 (async () => {
     const spaces = await getSpaces();
 
-    const onlyDefault = spaces.filter(space => {
-        return space.namespace === "[default]"
+    const onlyDefault = spaces.filter((space) => {
+        return space.namespace === "[default]";
     });
 
     // console.log(onlyDefault);
-
 
     downloadDatastore(onlyDefault);
 })();
